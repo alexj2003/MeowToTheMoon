fcaafcbcd5cc3927b019909126dbf7c4b3194a38
Alex Johns <alexanderj393@yahoo.com>
2025-03-21 22:44:26 +0000

    Add charged player jumps

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
+
+public enum PlayerState {
+    Idle,
+    Charging,
+    Jumping
+}
 
 public class PlayerController : MonoBehaviour
 {
-    float jumpSpeed = 8.0f;
-    float gravity = 9.8f * 16;
+    public Rigidbody2D rb;
+    public float minJumpForce = 5.0f;
+    public float maxJumpForce = 10.0f;
+    public float chargeRate = 10.0f;
+    public float jumpCharge = 0.0f;
+    public PlayerState state = PlayerState.Idle;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,17 +25,40 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Update vertical speed
-        float vertical = 0.0f;
-        if (Keyboard.current.spaceKey.isPressed) {
-            vertical = jumpSpeed;
+        // Input class isn't recommended for new projects but this is the simplest way for now
+
+        switch (state) {
+            case PlayerState.Idle:
+                if (Input.GetKeyDown(KeyCode.Space)) {
+                    // Start charging the jump
+                    state = PlayerState.Charging;
+                    jumpCharge = 0.0f;
+                }
+                break;
+            case PlayerState.Charging:
+                if (Keyboard.current.spaceKey.isPressed) {
+                    // Charge the jump if the space key is held
+                    jumpCharge += chargeRate * Time.deltaTime;
+                    jumpCharge = Mathf.Clamp(jumpCharge, 0.0f, maxJumpForce);
+                }
+                if (Input.GetKeyUp(KeyCode.Space)) {
+                    // Jump when the space key is released
+                    float jumpForce = Mathf.Max(minJumpForce, jumpCharge);
+                    
+                    // Reset the vertical velocity and apply the jump force
+                    rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0.0f);
+                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+                    state = PlayerState.Jumping;
+                    jumpCharge = 0.0f;
+                }
+                break;
+            case PlayerState.Jumping:
+                if (Mathf.Abs(rb.linearVelocity.y) < 0.01f) {
+                    // Change state back to idle when the player lands
+                    state = PlayerState.Idle;
+                }
+                break;
         }
-
-        // Apply gravity
-        // vertical -= gravity * Time.deltaTime;
-
-        // Move the player
-        Vector3 move = new Vector3(0, vertical, 0);
-        transform.Translate(move * Time.deltaTime);
     }
 }
