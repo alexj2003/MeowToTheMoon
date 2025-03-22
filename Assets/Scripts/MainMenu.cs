39a825e5e360b15f4041ec9230809be1508aaadb
Alex Johns <alexanderj393@yahoo.com>
2025-03-22 23:18:23 +0000

    Changes to cutscenes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    // Start the game
     public void PlayGame() {
+        Debug.Log("Playing game");
         SceneManager.LoadScene("Intro Scene");
+    }
+
+    // End the game, quit the application
+    public void EndGame() {
+        Debug.Log("Ending game");
+        Application.Quit();
     }
 }
